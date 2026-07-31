e2375ade52d153c216c2876d9e3e9ed2ed632621
DAAM <perleaste@yahoo.it>
2026-07-31 11:37:35 +0200

    Bump version to v0.081 across index files and editor

--- mapEditor/editor.js
+++ mapEditor/editor.js
@@ -802,7 +802,7 @@ function serializeMapData() {
   });
 
   return {
-    version: "v0.080",
+    version: "v0.081",
     playerSpawn: playerSpawn,
     carvedVoxels: world.carvedVoxels || {},
     objects: objectsMeta,
